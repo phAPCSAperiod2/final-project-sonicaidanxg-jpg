125273a3bb834e1a59e2bf8a7e3dc054d41b3ce8
sonicaidanxg-jpg <sonicaidanxg@gmail.com>
2026-04-23 17:40:30 +0000

    Testing

--- src/App.java
+++ src/App.java
@@ -25,5 +25,27 @@ public class App {
         for (Object quote : quoteManager.returnQuoteList()) {
             System.out.println((String) quote);
         }
+
+        // Test with empty list
+        Image imgManager = new Image(
+                "https://ohiodnr.gov/wps/wcm/connect/gov/4e9331d0-5d92-47dd-9ce2-04a2c62ea619/whitetaileddeer3.jpg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_JQGCH4S04P41206HNUKVF31000-4e9331d0-5d92-47dd-9ce2-04a2c62ea619-pIvo2PS",
+                "Image1");
+        imgManager.displayRandImage(); // Should print "No images to display."
+
+        // Add images
+        imgManager.addImage(new Image(
+                "https://ohiodnr.gov/wps/wcm/connect/gov/4e9331d0-5d92-47dd-9ce2-04a2c62ea619/whitetaileddeer3.jpg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_JQGCH4S04P41206HNUKVF31000-4e9331d0-5d92-47dd-9ce2-04a2c62ea619-pIvo2PS",
+                "Image1"));
+        imgManager.addImage(new Image(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQoS9T4c7jtJHvR749fPCCA1Uny4-3R2486w&s",
+                "Image2"));
+
+        // Display random image
+        imgManager.displayRandImage();
+
+        // Remove an image and display again
+        imgManager.removeImage(1);
+        imgManager.displayRandImage();
+
     }
 }
