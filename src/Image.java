2360a276bb6623e78dc5ebc693942bd51958124c
sonicaidanxg-jpg <sonicaidanxg@gmail.com>
2026-04-23 17:08:21 +0000

    Documentation for image class

--- src/Image.java
+++ src/Image.java
@@ -1,37 +1,69 @@
 import java.util.ArrayList;
 
-public class Image{
+public class Image {
 
     public static ArrayList<Image> ImageList = new ArrayList<>();
     private String imageLink;
     private String identifier;
 
-    public Image(String link, String id){
+    /*
+    Constructor for Image object.
+
+    @param link The web url of the image.
+    @param id A readable name to reference the image.
+    @author Aidan Manzanares
+    */
+
+
+    public Image(String link, String id) {
         imageLink = link;
         identifier = id;
     }
 
+    /*
+    Returns the ImageList
+    @return ArrayList
+    @author Aidan Manzanares
+    */
 
-    public ArrayList returnImageList(){
+
+    public ArrayList returnImageList() {
         return ImageList;
     }
 
+    /*
+    Uses a web url to display an image.
 
-    //wip
-    public void printRandImage(){
-        ImageList.get((int) Math.random()*((ImageList.size() - 1) + 1));
+    @author Aidan Manzanares
+    */
+
+
+    // wip
+    public void displayRandImage() {
+        Image f = ImageList.get((int) Math.random() * ((ImageList.size() - 1) + 1));
+        String link = f.imageLink;
+        System.out.println(link);
     }
 
+    /*
+    Adds an image object to the imageList.
 
-    public void addImage(Image I){
+    @author Aidan Manzanares
+    */
+
+    public void addImage(Image I) {
         ImageList.add(I);
     }
 
-    public void removeImage(int index){
+
+    /*
+    Removes an image from the imageList using an index starting at 1.
+
+    @author Aidan Manzanares
+    */
+
+    public void removeImage(int index) {
         ImageList.remove(index - 1);
     }
-
-
-
 
 }
