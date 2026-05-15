a3a1a2b5a6b240da4d5644dc071e726dbfc94207
sonicaidanxg-jpg <sonicaidanxg@gmail.com>
2026-05-15 00:29:45 +0000

    Initializing list values

--- src/App.java
+++ src/App.java
@@ -3,19 +3,19 @@ public class App {
 
         System.out.println("Initializing lists...");
 
-        Quote q = new Quote("First quote", "Author One");
-        q.addQuote("Second quote", "Author Two");
-        q.addQuote("Third quote");
+        Quote q = new Quote("Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.", "Roy T. Bennett");
+        q.addQuote("Learn the rules like a pro, so you can break them like an artist.", "Pablo Picasso");
+        q.addQuote("Live as if you were to die tomorrow. Learn as if you were to live forever.", "Mahatma Gandhi");
 
-        Image i = new Image("https://cdn12.picryl.com/photo/2016/12/31/deer-whitetail-fawn-animals-087653-1024.jpg",
-                "Image1");
+        Image i = new Image("https://cdn.printerval.com/unsafe/960x960/asset/111049560a1d1c1a0d1a1a141d56161d0c571115191f1d564c48484b404e4a4d4f5640414d4f571e14190c544f4d480054484f4d541e5508191c544f4d480049484848541e401e401e40560d495612081f",
+                "HangInThere");
         i.addImage(i);
         Image b = new Image(
-                "https://pixnio.com/free-images/fauna-animals/deers/young-red-deer-fawn-odocoileus-virginianus.jpg",
-                "Image2");
+                "https://live.staticflickr.com/383/20238882830_cd090b63df_z.jpg",
+                "TryMe");
         i.addImage(b);
         Image c = new Image(
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg7mqOU8dEnsBAUEoHPwHRv3ITz0uOGnC5Vw&s", "Image3");
+                "https://images.pexels.com/photos/6005487/pexels-photo-6005487.jpeg", "WeBecome");
         i.addImage(c);
 
         System.out.println("Lists initialized.");
